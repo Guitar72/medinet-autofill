c9906ca754d7f954e85e5e95851db72404a7e730
Guitar72 <dhabolero@gmail.com>
2026-06-15 20:59:21 +0700

    Add files via upload

---
+++ Medinet.user.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Medinet
+// @namespace    http://tampermonkey.net/
+// @version      6.15
+// @description  Nut Thao Tac Nhanh nam trong header + Thong tin hanh chinh auto-fill + Hoi benh va kham lam sang (phan loai nhom NCT) + Phim tat Shift+A an/hien nut + Auto-match anh benh nhan theo ten+nam sinh
+// @author       Auto-generated
+// @match        https://quanlyskcd.medinet.org.vn/*
+// @grant        GM_setClipboard
+// @grant        GM_openInTab
+// @updateURL    https://raw.githubusercontent.com/Guitar72/medinet-autofill/refs/heads/main/Medinet_user.meta.js
+// @downloadURL  https://raw.githubusercontent.com/Guitar72/medinet-autofill/refs/heads/main/Medinet_user.js
+// ==/UserScript==
