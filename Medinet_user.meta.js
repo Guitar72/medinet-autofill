09c564613533b99f8c095bd180ac9669b4d6c5c8
Guitar72 <dhabolero@gmail.com>
2026-06-22 16:22:56 +0700

    Add files via upload

---
+++ Medinet_user.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Medinet
+// @namespace    http://tampermonkey.net/
+// @version      6.22
+// @description  Nut Thao Tac Nhanh
+// @author       Auto-generated
+// @match        https://quanlyskcd.medinet.org.vn/*
+// @grant        GM_setClipboard
+// @grant        GM_openInTab
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        unsafeWindow
+// @require      https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js
+// @updateURL    https://raw.githubusercontent.com/Guitar72/medinet-autofill/refs/heads/main/Medinet.user.meta.js
+// @downloadURL  https://raw.githubusercontent.com/Guitar72/medinet-autofill/refs/heads/main/Medinet.user.js
+// ==/UserScript==
